2603d91b2c54d30fe31b6455f177c3fd619a8160
inlym <inlym@qq.com>
2021-07-09 15:02:40 +0800

    :tada: 项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # life-helper-frontend
+
 「我的个人助手」项目 Web 前端部分
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+
+  baseURL: 'https://api.lifehelper.com.cn',
+}
