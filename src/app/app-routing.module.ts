eb5236856008e9dbee3f0a0fce21dc726504fcc1
inlym <inlym@qq.com>
2021-07-13 14:45:28 +0800

    :sparkles: 新增 `主页` 模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [{ path: '', loadChildren: () => import('./homepage/homepage.module').then((m) => m.HomepageModule) }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/homepage/homepage.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './components/home/home.component'
+
+const routes: Routes = [{ path: '', component: HomeComponent }]
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class HomepageModule {}
