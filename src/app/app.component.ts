d8a6b2aa5512a775119d95f50c7795445e04a3ff
inlym <inlym@qq.com>
2021-07-13 16:55:50 +0800

    :fire: 移除 `AppComponent` HTML 文件，因为代码就一行，直接放入到 `template` 配置项中

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {}
