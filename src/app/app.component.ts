51a338657cb0d5c8b74a87ba184557456b937a4e
inlym <inlym@qq.com>
2021-07-13 14:50:20 +0800

    :fire: 删除根组件的样式文件（该组件无需样式）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = '我的个人助手'
-}
+export class AppComponent {}
