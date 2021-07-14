cfdcc57d07a310b1e96cb870e2c79d44c14a3420
inlym <inlym@qq.com>
2021-07-14 16:05:39 +0800

    :heavy_plus_sign: 引入 Angular Material 组件库

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { MainInterceptor } from './common/main.interceptor'
@@ -8,7 +9,7 @@ import { SignatureInterceptor } from './common/signature.interceptor'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: MainInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: SignatureInterceptor, multi: true },
