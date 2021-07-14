5fcffd10f00d61550a9779a02d22bacd484de60f
inlym <inlym@qq.com>
2021-07-14 17:01:43 +0800

    :sparkles: 开始使用组件，完成首页导航栏

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
 
@@ -7,6 +10,6 @@ const routes: Routes = [{ path: '', component: HomeComponent }]
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class HomepageModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,10 @@ export const environment = {
   production: true,
 
   baseURL: 'https://api.lifehelper.com.cn',
+
+  sign: {
+    key: '203960533',
+    secret: '5RmSMMk7Z1MqaSzQvsZfAzUPqjvGJssV',
+    debug: false,
+  },
 }
