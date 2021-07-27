6f591cd031db0b7b7f188b67d8577a8ea02d1d9b
inlym <inlym@qq.com>
2021-07-27 14:34:31 +0000

    :heavy_minus_sign: 移除 `@angular/material` 依赖

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,8 +1,5 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
-import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterModule, Routes } from '@angular/router'
 import { SharedModule } from '../shared/shared.module'
 import { WeatherModule } from '../weather/weather.module'
