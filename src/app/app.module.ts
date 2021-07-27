8c0295f0d85a3e05336bea3dd77632f98986d259
inlym <inlym@qq.com>
2021-07-27 14:48:23 +0000

    :heavy_plus_sign: 引入 `ng-zorro-antd` 依赖

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
@@ -8,7 +9,7 @@ import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CoreModule, BrowserAnimationsModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CoreModule, BrowserAnimationsModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
