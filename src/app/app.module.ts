c787bc9184d8f4147d80b5bebd1a0f0b92da2a6b
inlym <inlym@qq.com>
2021-07-27 10:01:40 +0000

    :building_construction: 将请求拦截器（`HTTP_INTERCEPTORS`）从根模块移除，改为从核心模块（`CoreModule`）引入，再将核心模块引入根模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { AuthInterceptor } from './common/interceptors/auth.interceptor'
-import { SignatureInterceptor } from './common/interceptors/signature.interceptor'
-import { UrlInterceptor } from './common/interceptors/url.interceptor'
 import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule, CoreModule],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: UrlInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: SignatureInterceptor, multi: true },
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CoreModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,17 @@
 import { CommonModule } from '@angular/common'
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { NgModule, Optional, SkipSelf } from '@angular/core'
+import { AuthInterceptor } from './interceptors/auth.interceptor'
+import { SignatureInterceptor } from './interceptors/signature.interceptor'
+import { UrlInterceptor } from './interceptors/url.interceptor'
 
 @NgModule({
   declarations: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: UrlInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: SignatureInterceptor, multi: true },
+  ],
   imports: [CommonModule],
 })
 export class CoreModule {
