52936f60c71258b32e0d2c77084743b0975949a2
inlym <inlym@qq.com>
2021-08-17 10:03:04 +0000

    :sparkles: 新增个人资料页

---
+++ src/app/user/profile/profile.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({ providedIn: 'root' })
+export class ProfileService {
+  constructor(private readonly http: HttpClient) {}
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { SharedModule } from '../shared/shared.module'
 import { UserHomeComponent } from './user-home/user-home.component'
+import { ProfileComponent } from './profile/profile.component'
 
-const routes: Routes = [{ path: '', component: UserHomeComponent }]
+const routes: Routes = [
+  {
+    path: '',
+    component: UserHomeComponent,
+
+    children: [
+      { path: 'profile', component: ProfileComponent },
+      { path: '**', redirectTo: 'profile' },
+    ],
+  },
+]
 
 @NgModule({
-  declarations: [UserHomeComponent],
+  declarations: [UserHomeComponent, ProfileComponent],
   imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
 })
 export class UserModule {}
