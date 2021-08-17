4f5da081955e1c9d3b370251f49bce0225f7491c
inlym <inlym@qq.com>
2021-08-17 08:22:51 +0000

    :children_crossing: 优化了页面样式

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,15 +1,19 @@
-import { CommonModule } from '@angular/common'
+import { CommonModule, registerLocaleData } from '@angular/common'
+import zh from '@angular/common/locales/zh'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { NzCalendarModule } from 'ng-zorro-antd/calendar'
+import { NzCarouselModule } from 'ng-zorro-antd/carousel'
 import { SharedModule } from '../shared/shared.module'
 import { WeatherModule } from '../weather/weather.module'
 import { HomeComponent } from './home/home.component'
-import { NzCarouselModule } from 'ng-zorro-antd/carousel'
+
+registerLocaleData(zh)
 
 const routes: Routes = [{ path: '', component: HomeComponent }]
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), WeatherModule, SharedModule, NzCarouselModule],
+  imports: [CommonModule, RouterModule.forChild(routes), WeatherModule, SharedModule, NzCarouselModule, NzCalendarModule],
 })
 export class HomepageModule {}
