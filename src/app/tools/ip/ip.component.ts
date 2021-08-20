461aba5afc16ca7fd86b974a608eda89e813a1f8
inlym <inlym@qq.com>
2021-08-20 03:26:04 +0000

    :sparkles: 新增 IP 定位查询功能

--- src/app/tools/ip/ip.component.ts
+++ src/app/tools/ip/ip.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { Address, IpResponse, Location } from './ip.interface'
+import { IpService } from './ip.service'
 
 @Component({
   selector: 'app-ip',
@@ -6,7 +8,39 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./ip.component.scss'],
 })
 export class IpComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly ipService: IpService) {}
 
-  ngOnInit(): void {}
+  public clientIp = ''
+  public queryIp = ''
+  public mapUrl = ''
+  public location!: Location
+  public address!: Address
+
+  ngOnInit(): void {
+    this.getClientIpInfo()
+  }
+
+  getClientIpInfo(): void {
+    this.ipService.getIpInfo().subscribe((data) => {
+      this.assign(data)
+    })
+  }
+
+  queryIpInfo(ip: string): void {
+    this.ipService.getIpInfo(ip).subscribe((data) => {
+      this.assign(data)
+    })
+  }
+
+  /**
+   * 对响应数据做赋值操作
+   *
+   * @param data 获取的响应数据
+   */
+  assign(data: IpResponse): void {
+    this.clientIp = data.clientIp
+    this.mapUrl = data.mapUrl
+    this.location = data.location
+    this.address = data.address
+  }
 }
