eab376cf457079ab109959c558c94d31f8906029
inlym <inlym@qq.com>
2022-07-04 19:47:42 +0800

    :heavy_plus_sign: 引入 `NGX Logger`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {HttpClientModule} from '@angular/common/http'
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
+import {LoggerModule, NgxLoggerLevel} from 'ngx-logger'
 
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
@@ -8,7 +9,17 @@ import {CoreModule} from './core/core.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, CoreModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    LoggerModule.forRoot({
+      level: NgxLoggerLevel.TRACE,
+      serverLoggingUrl: '',
+      serverLogLevel: NgxLoggerLevel.DEBUG,
+    }),
+    AppRoutingModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
