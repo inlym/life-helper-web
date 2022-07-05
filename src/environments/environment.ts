947a974007e3f0e82561d650f8f3de8ddc34e2ed
inlym <inlym@qq.com>
2022-07-05 23:25:18 +0800

    :building_construction: 引入私密配置文件

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,14 @@
+import {k, s} from 'life-helper-frontend-config'
+
 export const environment = {
   /** 是否为生产环境 */
   production: false,
 
   /** 服务端请求地址 */
   baseURL: 'https://api-local.lifehelper.com.cn',
+
+  /** 阿里云 API 网关签名密钥对 */
+  key: k,
+
+  secret: s,
 }
