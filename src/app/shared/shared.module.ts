86ba57daa954ee264c4b0b98fbbd8980c1c018e0
inlym <inlym@qq.com>
2022-07-06 20:47:55 +0800

    :sparkles: 新增导航栏组件

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
-import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+import {NgModule} from '@angular/core'
+import {MatIconModule} from '@angular/material/icon'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {HeaderComponent} from './header/header.component'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
 })
 export class SharedModule {}
