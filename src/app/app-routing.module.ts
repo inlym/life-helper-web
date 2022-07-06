642b6afc95144583a53c0a77c8fc5dbf555bd726
inlym <inlym@qq.com>
2022-07-06 21:30:54 +0800

    :construction: 开始写页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
+import {HomeComponent} from './single/home/home.component'
 
-const routes: Routes = []
+const routes: Routes = [{path: '', component: HomeComponent}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import {HttpClientModule} from '@angular/common/http'
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {LoggerModule, NgxLoggerLevel} from 'ngx-logger'
-
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {CoreModule} from './core/core.module'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {SharedModule} from './shared/shared.module'
+import {HomeComponent} from './single/home/home.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     LoggerModule.forRoot({level: NgxLoggerLevel.TRACE}),
     AppRoutingModule,
-    CoreModule,
     BrowserAnimationsModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
