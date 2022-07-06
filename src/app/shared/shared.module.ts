ca81dfdd8212662d3ece8a933781d60ca1fbf60a
inlym <inlym@qq.com>
2022-07-07 00:15:39 +0800

    :sparkles: 开始写首页

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import {NgModule} from '@angular/core'
 import {MatIconModule} from '@angular/material/icon'
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {HeaderComponent} from './header/header.component'
+import {MatButtonModule} from '@angular/material/button'
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule, MatIconModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
 export class SharedModule {}
