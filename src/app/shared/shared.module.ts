ffa4f89da995085a84207bd9aa28bd8457197850
inlym <inlym@qq.com>
2022-12-31 23:02:15 +0800

    :sparkles: 新增底栏

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {HeaderComponent} from './header/header.component'
+import {FooterComponent} from './footer/footer.component'
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
