88831437d1ddc3a8190f055c09be642f4d2e7d1f
inlym <inlym@qq.com>
2023-01-10 02:07:38 +0800

    :construction: 继续写「扫码登录」页

--- src/app/single/login/login.component.ts
+++ src/app/single/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core'
+import {Subscription, interval, mergeMap} from 'rxjs'
 import {LoginService} from './login.service'
 
 @Component({
@@ -16,8 +17,14 @@ export class LoginComponent implements OnInit {
   /** 小程序码图片的 URL 地址 */
   url = ''
 
-  /** 是否开启「下次自动登录」 */
-  autoLogin = true
+  /** 是否已扫码 */
+  scanned = false
+
+  /** 是否已失效 */
+  invalid = false
+
+  ob1?: Subscription
+  ob2?: Subscription
 
   constructor(private loginService: LoginService) {}
 
@@ -29,15 +36,40 @@ export class LoginComponent implements OnInit {
   private initPageData() {
     this.id = ''
     this.url = ''
+    this.scanned = false
+    this.invalid = false
+
+    if (this.ob1) {
+      this.ob1.unsubscribe()
+      this.ob1 = undefined
+    }
+
+    if (this.ob2) {
+      this.ob2.unsubscribe()
+      this.ob2 = undefined
+    }
   }
 
   /** 页面初始化方法 */
   private init(): void {
     this.initPageData()
 
-    this.loginService.getQrcode().subscribe((data) => {
+    this.ob1 = this.loginService.getQrcode().subscribe((data) => {
       this.id = data.id
       this.url = data.url
+
+      this.ob2 = interval(2000)
+        .pipe(
+          mergeMap((v) => {
+            console.log(v)
+            return this.loginService.check(this.id)
+          })
+        )
+        .subscribe((data) => {
+          if (data.scanned) {
+            this.scanned = true
+          }
+        })
     })
   }
 
