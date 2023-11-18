0d6411f7bc4f6cf0cf8580e036766c5aa83b815d
inlym <inlym@qq.com>
2023-11-18 20:34:26 +0800

    :construction: 新增主页

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet></router-outlet>',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'life-helper-web';
+  title = 'life-helper-web'
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomeComponent }]
