7673d24768b5907035d27d4ada88f06ad0eb428b
inlym <inlym@qq.com>
2023-11-21 15:24:36 +0800

    :heavy_plus_sign: 引入 `Angular Material` 库

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { ApplicationConfig } from '@angular/core'
+import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
 import { authInterceptor } from './common/interceptors/auth.interceptor'
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([urlInterceptor, authInterceptor, signatureInterceptor])),
+    provideAnimations(),
   ],
 }
