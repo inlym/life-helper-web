34b48a3ebd0eff7b54baa616154a6ee4d7615569
inlym <inlym@qq.com>
2023-12-02 15:51:07 +0800

    :tada: 开始写「待办清单」主页

---
+++ src/app/todo/todo-home/todo-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-home.component.html',
+  styleUrl: './todo-home.component.scss'
+})
+export class TodoHomeComponent {
+
+}
