15438ce0140cc4fedb9ac62b796527249bff85c1
inlym <inlym@qq.com>
2023-12-03 21:46:15 +0800

    :sparkles: 新增错误码处理拦截器

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,15 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
-import { ApplicationConfig } from '@angular/core'
-import { provideAnimations } from '@angular/platform-browser/animations'
-import { provideRouter } from '@angular/router'
-import { routes } from './app.routes'
-import { authInterceptor } from './common/interceptors/auth.interceptor'
-import { signatureInterceptor } from './common/interceptors/signature.interceptor'
-import { urlInterceptor } from './common/interceptors/url.interceptor'
+import {provideHttpClient, withInterceptors} from '@angular/common/http'
+import {ApplicationConfig} from '@angular/core'
+import {provideAnimations} from '@angular/platform-browser/animations'
+import {provideRouter} from '@angular/router'
+import {routes} from './app.routes'
+import {authInterceptor} from './common/interceptors/auth.interceptor'
+import {signatureInterceptor} from './common/interceptors/signature.interceptor'
+import {urlInterceptor} from './common/interceptors/url.interceptor'
+import {errorCodeInterceptor} from './common/interceptors/error-code.interceptor'
+
+const httpInterceptors = [urlInterceptor, authInterceptor, signatureInterceptor, errorCodeInterceptor]
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([urlInterceptor, authInterceptor, signatureInterceptor])),
-    provideAnimations(),
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors(httpInterceptors)), provideAnimations()],
 }
