4f08eb3a5164acf73e958d7f91d092496cf35ba5
inlym <inlym@qq.com>
2023-12-03 15:21:55 +0800

    :construction: 继续写待办清单模块

--- src/app/todo/todo-home/todo-home.component.ts
+++ src/app/todo/todo-home/todo-home.component.ts
@@ -1,26 +1,29 @@
 import {CommonModule} from '@angular/common'
 import {Component, OnInit} from '@angular/core'
-import {MatExpansionModule} from '@angular/material/expansion'
-import {MatGridListModule} from '@angular/material/grid-list'
-import {MatListModule} from '@angular/material/list'
-import {Project} from './todo.model'
+import {MatIconModule} from '@angular/material/icon'
+import {Project, Tag} from './todo.model'
 import {TodoService} from './todo.service'
 
 @Component({
   selector: 'app-todo-home',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, MatExpansionModule, MatListModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './todo-home.component.html',
   styleUrl: './todo-home.component.scss',
 })
 export class TodoHomeComponent implements OnInit {
   projectList: Project[] = []
+  tagList: Tag[] = []
 
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
     this.todoService.getProjectList().subscribe((data) => {
       this.projectList = data
+    })
+
+    this.todoService.getTagList().subscribe((data) => {
+      this.tagList = data
     })
   }
 }
--- src/app/todo/todo-home/todo.service.ts
+++ src/app/todo/todo-home/todo.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core'
-import {Project} from './todo.model'
+import {Project, Tag} from './todo.model'
 import {of} from 'rxjs'
 
 @Injectable({providedIn: 'root'})
@@ -11,6 +11,15 @@ export class TodoService {
       {id: 1, name: '2dfdsf'},
       {id: 2, name: '23432e'},
       {id: 3, name: '32424'},
+    ]
+    return of(list)
+  }
+
+  getTagList() {
+    const list: Tag[] = [
+      {id: 666, name: '标签3号'},
+      {id: 333, name: '标签2号'},
+      {id: 232, name: '标签1号'},
     ]
     return of(list)
   }
