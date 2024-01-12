e59bae496a7a99cc633cf03012f92e6e36e6b961
inlym <inlym@qq.com>
2024-01-12 22:05:26 +0800

    :construction: 新增开发中过渡页

---
+++ src/app/single/construction/construction.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core'
+import {CommonModule} from '@angular/common'
+
+// 说明
+// 部分模块仍在开发中，就直接跳转到这里作为过渡
+
+@Component({
+  selector: 'app-construction',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './construction.component.html',
+  styleUrl: './construction.component.scss',
+})
+export class ConstructionComponent {}
